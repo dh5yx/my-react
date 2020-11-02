5ca46057a3f0b690acae703ce2b84dca020b8b0e
denghong <denghong>
2020-11-02 18:00:22 +0800

    dh

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   };
   selectMenu(data) {
     console.log(data)
-    console.log(this.props)
+    console.log(this)
 
   }
   render() {
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 export default class about extends Component {
+    constructor(props){
+        super(props)
+        console.log(this.props)
+    }
+
     render() {
         return (
             <div>
