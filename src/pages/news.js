1298cdb2ec182fb5793f105b032639c082f56dd1
denghong <denghong>
2020-11-02 17:07:55 +0800

    dh

---
+++ src/pages/news.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class news extends Component {
+    render() {
+        return (
+            <div>
+                新闻
+            </div>
+        )
+    }
+}
